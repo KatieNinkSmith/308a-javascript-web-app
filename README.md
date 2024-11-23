8ae626e10035ce026d3641cfc4ba963f1be43083
Katie Nink Smith <katiej.smith@outlook.com>
2024-11-23 14:30:36 -0700

    drop downs filled with options

--- README.md
+++ README.md
@@ -12,3 +12,14 @@ Add /preview to the end of the meal image URL
 /images/media/meals/llcbn01574260722.jpg/preview
 
 https://media.istockphoto.com/id/877763072/photo/vintage-recipe-box.jpg?s=612x612&w=0&k=20&c=vKX9nEqY4cDLy8RegWuFhk9ITMGdPmKygiPLDoxGT7Y=
+#e7b40d yellow
+#00a6a6 teal
+#00cccc light teal
+#ffcc99 light brown
+#cc6600 orange
+#e8f7ee kinda white
+#add59a green
+
+/strMealThumb image
+/strMeal meal name
+/strCategory country name
--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,67 @@
-const mealSelect = document.querySelector("#mealDropDown");
+// export const buttons = document.querySelector(".btn");
+// console.log(buttons);
+export const filterByOrigin = document.querySelector("#originSelect");
+// console.log(filterByOrigin);
+export const filterByCategory = document.querySelector("#categorySelect");
+// console.log(filterByCategory);
+export const random = document.querySelector("#randomBtn");
+// console.log(random);
+export const mealSelect = document.querySelector("#mealDropDown");
+// console.log(mealSelect);
+export const recipeForm = document.querySelector("#racipeCard");
+console.log(recipeForm);
 
-console.log(mealSelect);
-const endPoints = {
-  meals: "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast",
-  mealIngredients: "https://www.themealdb.com/api/json/v1/1/list.php?i=list",
-  mealCatagory: `https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood`,
+export const endPoints = {
+  mealOrigin: `https://www.themealdb.com/api/json/v1/1/list.php?a=list`,
+  mealCat: `https://www.themealdb.com/api/json/v1/1/list.php?c=list`,
   random: `https://www.themealdb.com/api/json/v1/1/random.php`,
-  mealInfo: ``,
 };
+// console.log(endPoints);
 
-async function food() {
-  const data = await fetch(`${endPoints.mealCatagory}`);
+// adds all world orgin locations as a recipe filter option
+async function originSelect(event) {
+  const data = await fetch(`${endPoints.mealOrigin}`);
   // console.log(data);
   const jsonData = await data.json();
   // console.log(jsonData.meals, "stuff");
-  jsonData.meals.forEach((item) => {
+  const mealName = jsonData.meals;
+  mealName.forEach((item) => {
     const newElement = document.createElement("option");
-    newElement.value = item.mealId;
-    newElement.textContent = item.strMeal;
-    mealDropDown.appendChild(newElement);
+    newElement.value = item.strArea;
+    newElement.textContent = item.strArea;
+    filterByOrigin.appendChild(newElement);
     // console.log(newElement);
   });
-  const data1 = await fetch(`${endPoints.random}`);
-  // console.log(data);
-  const random = document.createElement("option");
-  random.value = data1.mealId;
-  random.textContent = "Random";
-  mealDropDown.appendChild(random);
-
-  mealSelect.addEventListener("change", async (event) => {
-    const data = await fetch(
-      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${event.target.mealId}`
-    );
-    const jsonData = await data.json();
-    console.log(jsonData, "stuff1");
-  });
-  console.log("your a banana");
 }
-food();
+originSelect();
+
+// adds meal catagories as a filter option
+async function categorySelect(event) {
+  const data = await fetch(`${endPoints.mealCat}`);
+  // console.log(data);
+  const jsonData = await data.json();
+  // console.log(jsonData.meals, "stuff");
+  const mealName = jsonData.meals;
+  mealName.forEach((item) => {
+    const newElement = document.createElement("option");
+    newElement.value = item.strCategory;
+    newElement.textContent = item.strCategory;
+    filterByCategory.appendChild(newElement);
+    // console.log(newElement);
+  });
+}
+categorySelect();
+// console.log("your a banana");
+
+// adds random recipe for an optins to the meal options
+async function randomRecipe(event) {
+  const data1 = await fetch(`${endPoints.random}`);
+  const ranRecipe = document.createElement("option");
+  ranRecipe.value = data1.strMeal;
+  ranRecipe.textContent = "Random Recipe";
+  mealDropDown.appendChild(ranRecipe);
+  const recipeCard = document.createElement("p");
+  recipeCard.id = data1.strMeal;
+  recipeCard.innerHTML = data1.strInstructions;
+}
+randomRecipe();
